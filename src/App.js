20ebc581aa6c976817875cd0178d194d378ad356
Magda Sz <pesahson@gmail.com>
2021-06-20 17:26:38 +0200

    add dark/light mode

--- src/App.js
+++ src/App.js
@@ -12,19 +12,48 @@ const getLocalStorage = () => {
   }
 }
 
+const getStorageMode = () => {
+  let mode = 'light-mode';
+  if(localStorage.getItem('mode')){
+    mode = localStorage.getItem('mode')
+  }
+  return mode;
+}
+
+
 
 function App() {
   const [todos, setTodos] = useState(getLocalStorage());
   const [userInput, setUserInput] = useState('');
+  const [mode, setMode] = useState(getStorageMode())
+
+  
+const toggleMode = () => {
+  if (mode === "light-mode") {
+    setMode("dark-mode");
+  } else {
+    setMode("light-mode");
+  }
+};
+
+  useEffect(() => {
+    document.documentElement.className = mode;
+    localStorage.setItem('mode', mode)
+  }, [mode])
 
   useEffect(() => {
     localStorage.setItem('list', JSON.stringify(todos))
   }, [todos])
+
+
   return (
+    <div class="container">
     <main>
       <Form setTodos={setTodos} todos={todos} userInput={userInput} setUserInput={setUserInput}/>
       <TodoList todos={todos} setTodos={setTodos}/>
+      <button onClick={toggleMode}>theme</button>
     </main>
+    </div>
   );
 }
 
